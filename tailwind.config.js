fd71a57db1120de3820a48794acc7327e5e9efa9
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2023-09-16 18:08:17 -0400

    creando el proyecto base

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#131424',
+        secondary: '#393A47',
+        accent: '#F13024',
+      },
+      backgroundImage: {
+        explosion: 'url("./src/images/bg-explosion.png")',
+        circles: 'url("./src/images/bg-circles.png")',
+        circleStar: 'url("./src/images/circle-star.svg")',
+        site: 'url("./src/images/site-bg.svg")',
+      },
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+      },
+      fontFamily: {
+        poppins: [`var(--font-poppins)`, 'sans-serif'],
+        sora: [`var(--font-sora)`, 'sans-serif'],
+      },
+    },
+  },
+  container: {
+    padding: {
+      DEFAULT: '15px',
+    },
+  },
+  plugins: [require('tailwind-scrollbar')],
+}
+
+
